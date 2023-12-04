87c0253ef16e2e84b560920e957a92386e3909f0
Cameron Smith <smithc11@rpi.edu>
2023-12-04 15:55:25 -0500

    using jigsaw 1.0.0

---
+++ envGcc12.sh
@@ -0,0 +1,5 @@
+module use /opt/scorec/spack/rhel9/v0201_4/lmod/linux-rhel9-x86_64/Core/
+module load gcc/12.3.0-iil3lno mpich/4.1.1-xpoyz4t
+module load python/3.10.10-fy3aixq cmake
+
+source /space/cwsmith/compassLandice/jigsaw-python/pyEnv/bin/activate
